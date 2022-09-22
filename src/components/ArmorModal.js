edaa6102561169410e68d92f17207dff4f405acf
Lucas <lucaswarwick02@gmail.com>
2022-09-22 10:53:02 +0100

    Add text filter + sort option to ArmorModal

--- src/components/ArmorModal.js
+++ src/components/ArmorModal.js
@@ -1,10 +1,13 @@
-import React from 'react';
+import React, { useState } from 'react';
 import ArmorItem from './ArmorItem'
 import { headData, chestData, legsData} from './JsonManager'
-import { originalSort } from "../HelperFunctions"
+import { originalSort, sortByName, sortByRarity } from "../HelperFunctions"
 import '../css/ArmorModal.css';
 
 function ArmorModal(props) {
+
+    const [filterText, setFilterText] = useState("");
+    const [sortType, setSortType] = useState("None");
 
     /**
      * Removes the Modal
@@ -30,6 +33,29 @@ function ArmorModal(props) {
         }
     }
 
+    const getFilteredItems = () => {
+        const sortedItems = getItems().sort(sortFunction(sortType));
+
+        return sortedItems.filter(item => {
+            const mainString = item.name.toLowerCase();
+            const substring = filterText.toLowerCase();
+            return mainString.includes(substring);
+        });
+    };
+
+    const sortFunction = sortString => {
+        switch (sortString) {
+            case "None":
+                return originalSort;
+            case "Name":
+                return sortByName;
+            case "Rarity":
+                return sortByRarity;
+            default:
+                return originalSort;
+        }
+    }
+
     /**
      * Updates the stored armor information
      * @param event - From the HTMLElement, used for determining values
@@ -45,9 +71,15 @@ function ArmorModal(props) {
     return (
         <div id="armorModal">
             <div id="armorModalContent">
-                <h1>Armor Modal</h1>
+                <h1 id="armorModalHeading">Armor Modal</h1>
+                <div id="filter">
+                    <input id="filterByText" type="text" size="30" onChange={event => {setFilterText(event.target.value)}} />
+                    <input type="radio" name="sortBy" onClick={() => { setSortType("None"); }} /> None
+                    <input type="radio" name="sortBy" onClick={() => { setSortType("Name"); }} /> Name
+                    <input type="radio" name="sortBy" onClick={() => { setSortType("Rarity"); }} /> Rarity
+                </div>
                 <div className="selectionContainer">
-                    {getItems().map(item => (
+                    {getFilteredItems().map(item => (
                         <ArmorItem key={item.itemID} onClick={() => handleArmorChange(props.modalType, item.itemID)} item={item} />
                     ))}
                 </div>
